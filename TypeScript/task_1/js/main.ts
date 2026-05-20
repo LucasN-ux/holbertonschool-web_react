bfee9e2c72e5f5f70507141e7c6c5c51135b5927
LucasN-ux <lucas.n230896@gmail.com>
2026-05-20 17:14:13 +0200

    feat: Add StudentClass and interfaces for student functionality

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -35,3 +35,30 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new(firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
